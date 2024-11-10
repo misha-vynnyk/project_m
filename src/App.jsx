0e238f1ce00f90e649ce21e76f3f57940165ef58
misha-vynnyk <vinnikmisha94@gmail.com>
2024-11-10 15:31:51 +0100

    added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./components/global_styles/Global";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import { useSidebar } from "./hooks/useSidebar";
 
-function App() {
-  const [count, setCount] = useState(0)
+const theme = {
+  mobile: "768px",
+  tablet: "1280px",
+  desktop: "1440px",
+
+  fonts: {
+    primaryFont: "'Inter', sans-serif",
+  },
+
+  colors: {
+    primaryLightBackground: "#FFFFFFF",
+    secondaryLightBackground: "#FFFFFF",
+    primaryDarkBackground: "#0D062D",
+    secondaryDarkBackground: "#141416",
+
+    borderLineColor: "1px solid rgb(219, 219, 219)",
+
+    darkText: "#0D062D",
+    lightText: "#7A7A7A",
+  },
+};
+
+const App = () => {
+  const [isSidebarOpen, toggleSidebar] = useSidebar();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header 
+        onToggleSidebar={toggleSidebar} 
+        isSidebarOpen={isSidebarOpen}
+      />
+      <Main isSidebarOpen={isSidebarOpen} />
+    </ThemeProvider>
+  );
+};
 
-export default App
+
+export default App;
